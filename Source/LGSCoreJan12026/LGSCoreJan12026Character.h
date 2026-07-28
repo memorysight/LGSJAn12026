4dc971eb97b47d19115a602c83a2b869fc258971
Daniel Smith <memorysight@gmail.com>
2026-07-28 16:20:50 -0400

    CleanC++BuildInLAB__7_28

--- Source/LGSCoreJan12026/LGSCoreJan12026Character.h
+++ Source/LGSCoreJan12026/LGSCoreJan12026Character.h
@@ -250,7 +250,7 @@ public:
 	FVector2D LastMoveInput = FVector2D::ZeroVector;
 	//end 4_14
 
-	//new 7_14 OmegaDrive First Pass
+	//new 7_14 OmegaDrive First Pass__ClothC++7_28Update
 	UFUNCTION()
 	void OnOmegaDrivePressed();
 
